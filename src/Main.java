d2a17050232700d7dea2b02bedf6cd46f74c0870
olvuk <radenovic-family@yandex.ru>
2023-05-05 14:22:54 +0200

    Выполенена Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,33 @@
 public class Main {
+    public static void checkYear(int year) {
+        if ((year % 4) != 0) {
+            System.out.println(year + " год не является високосным");
+        } else if ((year % 400) == 0) {
+            System.out.println(year + " год является високосным");
+        } else if ((year % 100) == 0) {
+            System.out.println(year + " год не является високосным");
+        } else {
+            System.out.println(year + " год является високосным");
+        }
+}
     public static void main(String[] args) {
         task1();
         task2();
         task3();
     }
+
     public static void task1() {
         System.out.println("Задача 1");
+        checkYear(2100);
     }
+
     public static void task2() {
         System.out.println();
         System.out.println("Задача 2");
+
     }
+
     public static void task3() {
-        System.out.println();
-        System.out.println("Задача 3");
+
     }
 }
